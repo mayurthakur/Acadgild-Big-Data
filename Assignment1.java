d6da5742dde86ed81e21a1165c011deea0059998
mayurthakur <mayurthakur91@gmail.com>
2016-05-26 23:56:27 +0530

    changes are  made to test the github

--- Assignment1.java
+++ Assignment1.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	int a =10,d=4;
 	float b=34.5f;
 	int c=(int)(a+b);
+/* Added comment to test the github */
 	System.out.println("The value of C is "+c);
 	System.out.println("The value of b is "+b);
 	System.out.println("The value of a is "+a
