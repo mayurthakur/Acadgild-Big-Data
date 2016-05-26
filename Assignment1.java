a03863909573845eb62252cdeae82d1708251382
mayurthakur <mayurthakur91@gmail.com>
2016-05-27 00:01:55 +0530

    changes are made to test deletion

--- Assignment1.java
+++ Assignment1.java
@@ -3,7 +3,6 @@ package FirstPrograms;
 public class Assignment1 {
 public static void main(String[] args) {
 	int a =10,d=4;
-	float b=34.5f;
 	int c=(int)(a+b);
 /* Added comment to test the github */
 	System.out.println("The value of C is "+c);
